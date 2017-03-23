481dd30a220388f14c93ff1f16a8e4fc365e2cf9
Ilia Karmanov <ilkarman@microsoft.com>
2017-03-23 00:42:05 -0400

    Removed debug at end

--- r_net.R
+++ r_net.R
@@ -193,46 +193,9 @@ backprop <- function(x, y, biases, weights)
 }
 
 # TODO!
-evaluate <- function(test_data)
-
-
-###############################
-## EVALUATE (Compare to Python)
-## I feed in the below values to python and
-## check that R script matches
-###############################
-
-# sizes <- c(3, 5, 2)
-# 
-# biases <- list(
-#   matrix(c(-0.28080838, 
-#            0.2316751,
-#            0.87261225,
-#            -0.96765989,
-#            -1.868544), 
-#          nrow=5, ncol=1, byrow=TRUE),
-#   matrix(c(2.10775394,
-#            0.41855275), 
-#          nrow=2, ncol=1, byrow=TRUE)
-#   )
-# 
-# weights <- list(
-#   matrix(c(-0.81267026, 0.17627318, -0.60639905, 
-#            0.50974091, 2.34693197, 0.33875867,
-#            -1.20632438, -1.25457351, -1.17803266,
-#            0.06163412, 0.61925722,  0.87939343, 
-#            -0.41764508, -0.28984466,  0.09663896), 
-#          nrow=5, ncol=3, byrow = TRUE),
-#   matrix(c(0.37480004, 0.04123139, 1.5200263, -2.02504715, 0.2665885, 
-#            1.1946554, 0.18426967, -0.16337889, -0.91305046, 0.05401374), 
-#          nrow=2, ncol=5, byrow = TRUE))
-
-# Should be:
-#(2, 3)
-#[[ 0.92956719  0.92438907  0.91830857],
-#[ 0.64309165  0.6703279   0.63000043]]
-#result_test <- feedforward(0.5)
-#dim(result_test)
-#result_test # CORRECT!
+evaluate <- function(test_data) 
+{
+  
+}
 
 
