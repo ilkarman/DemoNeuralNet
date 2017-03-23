63d828d69cc85758b950ecf88462f40a594b677b
Ilia Karmanov <ilkarman@microsoft.com>
2017-03-23 00:00:10 -0400

    typo

--- r_net.R
+++ r_net.R
@@ -79,6 +79,9 @@ update_mini_batch <- function(mini_batch, lr)
     delta_nabla_w <- delta_nablas[[-1]]
     
     # These don't look correct
+    # The python is:
+    # nabla_b = [nb+dnb for nb, dnb in zip(nabla_b, delta_nabla_b)]
+    # nabla_w = [nw+dnw for nw, dnw in zip(nabla_w, delta_nabla_w)]
     nabla_b <- lapply(seq_along(biases),function(j)
       unlist(nabla_b[j])+unlist(delta_nabla_b[j]))
     nabla_w <- lapply(seq_along(weights),function(j)
