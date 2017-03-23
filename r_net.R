026a553184a4782ba4ec195590372cdd6d0df699
Ilia Karmanov <ilkarman@microsoft.com>
2017-03-23 07:59:37 -0400

    Comments
    Added descriptive comments

--- r_net.R
+++ r_net.R
@@ -1,15 +1,13 @@
-# Seems to actually run ... happy surprise
-
-# NOTE to self: 
-#1. [-1] in R removes first element NOT takes last
-#2. for (i in 1:2) in R does include 2
+# neuralnetsimple.R
+# Neural Network from scratch in R
+# Ilia 23.03.2017
 
 ################################################
 ## Load Data (rough, rewrite as loader function)
 ################################################
 library(caret)
 
-data_input <- as.data.frame(data(iris))
+data_input <- as.data.frame(iris)
 
 # X (scale)
 scalemax <- function(x){x/max(x)}
@@ -18,13 +16,13 @@ x_data <- as.list(as.data.frame(t(scalemax(data_input[c(1:4)]))))
 dmy <- dummyVars(" ~ Species", data=data_input)
 y_data <- as.list(as.data.frame(t(predict(dmy, newdata = data_input))))
 
-# Create full data vector
+# Create full data vector (combining the X and y)
 all_data <- list()
 for (i in 1:length(x_data)){
   all_data[[i]] <- c(x_data[i], y_data[i])
 }
 
-# Shuffle
+# Shuffle before splitting
 all_data <- sample(all_data)
 # Split to training and test
 training_data <- all_data[1:100]
@@ -33,6 +31,9 @@ testing_data <- all_data[101:150]
 ################################################
 ## RUN
 ################################################
+
+# Step 1. Initialise nueral network (bias and weights for layers)
+# Tested only with one hidden layer
 create_neural_net <- neuralnetwork(c(4, 6, 3))
 
 sizes <- create_neural_net[[1]]
@@ -40,6 +41,7 @@ num_layers <- create_neural_net[[2]]
 biases <- create_neural_net[[3]]
 weights <- create_neural_net[[4]]
 
+# Step 2. Train NN using SGD
 trained_net <- SGD(training_data=training_data,
                    epochs=1000, 
                    mini_batch_size=10,
@@ -55,47 +57,43 @@ weights <- trained_net[[-1]]
 #print(weights)
 
 # Accuracy
-evaluate(testing_data, biases, weights)  # 0.96
+evaluate(testing_data, biases, weights)  # 0.98
 
 ################################################
 ## FUNCTIONS
 ################################################
 
+# Calculate activation function
 sigmoid <- function(z){1.0/(1.0+exp(-z))}
 
+# Partial derivative of activation function
 sigmoid_prime <- function(z){sigmoid(z)*(1-sigmoid(z))}
 
+# Cost function derivative
 cost_derivative <- function(output_activations, y){output_activations-y}
 
 # Init neural-network
 neuralnetwork <- function(sizes)
 {
   num_layers <- length(sizes)
+  # Gaussian distribution for biases and weights
   biases <- sapply(sizes[-1], function(f) {matrix(rnorm(n=f), nrow=f, ncol=1)})
   weights <- sapply(list(sizes[1:length(sizes)-1], sizes[-1]), function(f) {
     matrix(rnorm(n=f[1]*f[2]), nrow=f[2], ncol=f[1])})
-  # DEBUG
-  # Initialise at 0.5 to compare to python script
-  #biases <- sapply(sizes[-1], function(f) {matrix(0.5, nrow=f, ncol=1)})
-  #weights <- sapply(list(sizes[1:length(sizes)-1], sizes[-1]), function(f) {
-  #  matrix(0.5, nrow=f[2], ncol=f[1])})
-  # DEBUG
   # Return
   list(sizes, num_layers, biases, weights)
 }
 
-# FF for scoring
+# Feedforward input to get prediction (for socring)
 feedforward <- function(a)
 {
   for (f in 1:length(biases)){
     a <- matrix(a, nrow=length(a), ncol=1)
     b <- biases[[f]]
     w <- weights[[f]]
-    # Seriously? All I want to replicate is:
     # (py) a = sigmoid(np.dot(w, a) + b)
-    
     # Equivalent of python np.dot(w,a)
-    w_a <- if(is.null(dim(a))) w*a else w%*%a
+    w_a <- w%*%a
     # Need to manually broadcast b to conform to np.dot(w,a)
     b_broadcast <- matrix(b, nrow=dim(w_a)[1], ncol=dim(w_a)[-1])
     a <- sigmoid(w_a + b_broadcast)
@@ -103,20 +101,19 @@ feedforward <- function(a)
   a
 }
 
-# Complete (and tested up to this point) ...
-
-# Ignore validation data for now
+# Train network using stochastic-gradient descent
 SGD <- function(training_data, epochs, mini_batch_size, lr, biases, weights)
 {
-  n <- length(training_data)
+  # Every epoch
   for (j in 1:epochs){
-    # Stochastic mini-batch
+    # Stochastic mini-batch (shuffle data)
     training_data <- sample(training_data)
     # Partition set into mini-batches
     mini_batches <- split(training_data, 
                           ceiling(seq_along(training_data)/mini_batch_size))
-    # Feed forward all mini-batch
+    # Feed forward (and back) all mini-batches
     for (k in 1:length(mini_batches)) {
+      # Update biases and weights
       res <- update_mini_batch(mini_batches[[k]], lr, biases, weights)
       # Logging
       #cat("Updated: ", k, " mini-batches")
@@ -126,14 +123,14 @@ SGD <- function(training_data, epochs, mini_batch_size, lr, biases, weights)
     # Logging
     cat("Epoch: ", j, " complete")
   }
-  # Return
+  # Return trained biases and weights
   list(biases, weights)
 }
 
 update_mini_batch <- function(mini_batch, lr, biases, weights)
 {
   nmb <- length(mini_batch)
-  # Initialise updates with zero vectors
+  # Initialise updates with zero vectors (for EACH mini-batch)
   nabla_b <- sapply(sizes[-1], function(f) {matrix(0, nrow=f, ncol=1)})
   nabla_w <- sapply(list(sizes[1:length(sizes)-1], sizes[-1]), function(f) {
     matrix(0, nrow=f[2], ncol=f[1])})
@@ -141,12 +138,13 @@ update_mini_batch <- function(mini_batch, lr, biases, weights)
   for (i in 1:nmb){
     x <- mini_batch[[i]][[1]]
     y <- mini_batch[[i]][[-1]]
-    # Back propogatoin will return delta
+    # Back propogation will return delta
+    # Backprop for each obeservation in mini-batch
     delta_nablas <- backprop(x, y, biases, weights)
     delta_nabla_b <- delta_nablas[[1]]
     delta_nabla_w <- delta_nablas[[-1]]
-    
-    # The python is:
+    # Add on deltas to nabla
+    # The python equivalent for below is:
     # nabla_b = [nb+dnb for nb, dnb in zip(nabla_b, delta_nabla_b)]
     # nabla_w = [nw+dnw for nw, dnw in zip(nabla_w, delta_nabla_w)]
     nabla_b <- lapply(seq_along(biases),function(j)
@@ -154,13 +152,9 @@ update_mini_batch <- function(mini_batch, lr, biases, weights)
     nabla_w <- lapply(seq_along(weights),function(j)
       unlist(nabla_w[[j]])+unlist(delta_nabla_w[[j]]))
   }
-  
+  # After mini-batch has finished update biases and weights:
+  # i.e. weights = weights - (learning-rate/numbr in batch)*nabla_weights
   # Opposite direction of gradient
-  # TODO!!
-  # This should overwrite global (i.e. self.weights)
-  # I understand below is wrong, but need help to rewrite as OO
-  # Hmm I think these are wrong?
-  # Fix thesE:
   weights <- lapply(seq_along(weights), function(j)
     unlist(weights[[j]])-(lr/nmb)*unlist(nabla_w[[j]]))
   biases <- lapply(seq_along(biases), function(j)
@@ -169,14 +163,14 @@ update_mini_batch <- function(mini_batch, lr, biases, weights)
   list(biases, weights)
 }
 
+# Return gradient updates using back-propogation
 backprop <- function(x, y, biases, weights)
 {
-
   # Initialise updates with zero vectors
   nabla_b_backprop <- sapply(sizes[-1], function(f) {matrix(0, nrow=f, ncol=1)})
   nabla_w_backprop <- sapply(list(sizes[1:length(sizes)-1], sizes[-1]), function(f) {
     matrix(0, nrow=f[2], ncol=f[1])})
-  
+  # First:
   # Feed-forward (get predictions)
   activation <- matrix(x, nrow=length(x), ncol=1)
   activations <- list(matrix(x, nrow=length(x), ncol=1))
@@ -186,19 +180,18 @@ backprop <- function(x, y, biases, weights)
   for (f in 1:length(biases)){
     b <- biases[[f]]
     w <- weights[[f]]
-    
     w_a <- w%*%activation
     b_broadcast <- matrix(b, nrow=dim(w_a)[1], ncol=dim(w_a)[-1])
     z <- w_a + b
     zs[[f]] <- z
     activation <- sigmoid(z)
-    # Activations already contains init element
-    activations[[f+1]] <- activation
+    activations[[f+1]] <- activation  # Activations already contain one element
   }
-  
+  # Second:
   # Backwards (update gradient using errors)
   # Last layer
   sp <- sigmoid_prime(zs[[length(zs)]])
+  # Gradients update depending on how big the error in prediction was
   delta <- cost_derivative(activations[[length(activations)]], y) * sp
   nabla_b_backprop[[length(nabla_b_backprop)]] <- delta
   nabla_w_backprop[[length(nabla_w_backprop)]] <- delta %*% t(activations[[length(activations)-1]])
@@ -215,10 +208,12 @@ backprop <- function(x, y, biases, weights)
   return_nabla
 }
 
+# Evaluate test-data
 evaluate <- function(testing_data, biases, weights)
 {
   predictions <- list()
   truths <- list()
+  # Probably can avoid the for-loop here but this function run only once
   for (i in 1:length(testing_data)){
     test_data_chunk <- testing_data[[i]]
     test_x <- test_data_chunk[[1]]
@@ -226,7 +221,7 @@ evaluate <- function(testing_data, biases, weights)
     predictions[i] <- which.max(feedforward(test_x))
     truths[i] <- which.max(test_y)
   }
-  correct <-sum(mapply(function(x,y) x==y, predictions, truths))
+  correct <- sum(mapply(function(x,y) x==y, predictions, truths))
   total <- length(testing_data)
   # Return accuracy
   correct/total
