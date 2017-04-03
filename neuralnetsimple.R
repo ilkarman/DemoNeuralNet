96d4bc6c05bdad43fa066b4f44208bde38a41977
Ilia Karmanov <ilkarman@microsoft.com>
2017-04-03 19:35:26 +0100

    MNIST example complete
    Fixed some broken references where variables had to be used from global
    scope; add scaling to MNIST data.

--- neuralnetsimple.R
+++ neuralnetsimple.R
@@ -2,28 +2,20 @@
 # Neural Network from scratch in R
 # Ilia 03.04.2017
 
-##########
-# Issues:
-# 1. Running Breast-Cancer after iris doesn't seem to work well (as if something remains in memory from iris)
-##########
-
-##########
 # To-do:
-# 1. Add Cross-Entropy loss
 # 2. Add regularization
 # 3. Add ReLU and Tanh activations
 # 4. Allow saving/loading trained models
 # 5. Include more comments
 # 6. Include example on MNIST
 # 7. Allow better logging
-##########
 
 ################################################
 ## DATA FUNCTIONS
 ################################################
 
 # Return train, test lists in format for NN from input-dataframe
-train_test_from_df <- function(df, predict_col_index, train_ratio)
+train_test_from_df <- function(df, predict_col_index, train_ratio, scale_input=TRUE)
 {
   # Helper functions
   # Function to encode factor column as N-dummies
@@ -49,10 +41,13 @@ train_test_from_df <- function(df, predict_col_index, train_ratio)
   scalemax <- function(df)
   {
     numeric_columns <- which(sapply(df, is.numeric))
-    if (length(numeric_columns)){df[numeric_columns] <- lapply(df[numeric_columns], function(x){x/max(x)})}
+    if (length(numeric_columns)){df[numeric_columns] <- lapply(df[numeric_columns], function(x){
+      denom <- ifelse(max(x)==0, 1, max(x))
+      x/denom
+    })}
     df
   }
-  
+
   # Function to convert df to list of rows
   listfromdf <- function(df){as.list(as.data.frame(t(df)))}
   
@@ -60,7 +55,11 @@ train_test_from_df <- function(df, predict_col_index, train_ratio)
   # Omit NAs (allow other options later)
   df <- na.omit(df)
   # Get list for X-data
-  X_data <- listfromdf(dmy(scalemax(df[-c(predict_col_index)])))
+  if (scale_input){
+    X_data <- listfromdf(dmy(scalemax(df[-c(predict_col_index)])))
+  } else {
+    X_data <- listfromdf(dmy(df[-c(predict_col_index)]))
+  }
   # Get list for y-data
   y_data <- listfromdf(dmy(df[c(predict_col_index)]))
   # Combine X,y
@@ -77,7 +76,7 @@ train_test_from_df <- function(df, predict_col_index, train_ratio)
 }
 
 ################################################
-## NN FUNCTIONS
+## MATHS FUNCTIONS
 ################################################
 
 # Calculate activation function
@@ -86,8 +85,22 @@ sigmoid <- function(z){1.0/(1.0+exp(-z))}
 # Partial derivative of activation function
 sigmoid_prime <- function(z){sigmoid(z)*(1-sigmoid(z))}
 
+################################################
+## NN FUNCTIONS
+################################################
+
 # Cost function derivative
-cost_derivative <- function(output_activations, y){output_activations-y}
+#cost_derivative(method='mse', z=zs[[length(zs)]], a=activations[[length(activations)]], y=y)
+cost_delta <- function(method, z, a, y)
+{
+  if (method=='mse'){
+    # COST: 0.5*linalg.norm(a-y)**2
+    return (a-y)*sigmoid_prime(z)
+  } else if(method=='ce'){
+    # COST: sum(-y*log(a)-(1-y)*log(1-a))
+    return (a-y)
+  }
+}
 
 # Init neural-network
 neuralnetwork <- function(sizes)
@@ -112,7 +125,7 @@ neuralnetwork <- function(sizes)
 }
 
 # Feedforward input to get prediction (for socring)
-feedforward <- function(a)
+feedforward <- function(a, biases, weights)
 {
   for (f in 1:length(biases)){
     a <- matrix(a, nrow=length(a), ncol=1)
@@ -129,7 +142,7 @@ feedforward <- function(a)
 }
 
 # Train network using stochastic-gradient descent
-SGD <- function(training_data, epochs, mini_batch_size, lr, biases, weights)
+SGD <- function(training_data, testing_data, epochs, mini_batch_size, lr, C, sizes, num_layers, biases, weights, verbose=FALSE)
 {
   # Every epoch
   for (j in 1:epochs){
@@ -141,21 +154,25 @@ SGD <- function(training_data, epochs, mini_batch_size, lr, biases, weights)
     # Feed forward (and back) all mini-batches
     for (k in 1:length(mini_batches)) {
       # Update biases and weights
-      res <- update_mini_batch(mini_batches[[k]], lr, biases, weights)
+      res <- update_mini_batch(mini_batches[[k]], lr, C, sizes, num_layers, biases, weights)
       # Logging
-      #cat("Updated: ", k, " mini-batches")
+      #if (verbose){cat("Updated: ", k, " mini-batches")}
       biases <- res[[1]]
       weights <- res[[-1]]
     }
-    # Logging
-    #cat("Epoch: ", j, " complete")
+    # Logging every 5 epochs
+    if(verbose){if(j %% 1 == 0){
+      cat("Epoch: ", j, " complete")
+      # Print acc and hide confusion matrix
+      confusion <- evaluate(testing_data, biases, weights)
+      }}
   }
   cat("Training complete")
   # Return trained biases and weights
   list(biases, weights)
 }
 
-update_mini_batch <- function(mini_batch, lr, biases, weights)
+update_mini_batch <- function(mini_batch, lr, C, sizes, num_layers, biases, weights)
 {
   nmb <- length(mini_batch)
   listw <- sizes[1:length(sizes)-1] 
@@ -178,7 +195,7 @@ update_mini_batch <- function(mini_batch, lr, biases, weights)
     y <- mini_batch[[i]][[-1]]
     # Back propogation will return delta
     # Backprop for each obeservation in mini-batch
-    delta_nablas <- backprop(x, y, biases, weights)
+    delta_nablas <- backprop(x, y, C, sizes, num_layers, biases, weights)
     delta_nabla_b <- delta_nablas[[1]]
     delta_nabla_w <- delta_nablas[[-1]]
     # Add on deltas to nabla
@@ -193,6 +210,9 @@ update_mini_batch <- function(mini_batch, lr, biases, weights)
   # After mini-batch has finished update biases and weights:
   # i.e. weights = weights - (learning-rate/numbr in batch)*nabla_weights
   # Opposite direction of gradient
+  # l2 is L2 regularization
+  #weights <- lapply(seq_along(weights), function(j)
+  #  (1-lr*(l2/nmb_total))*unlist(weights[[j]])-(lr/nmb)*unlist(nabla_w[[j]]))
   weights <- lapply(seq_along(weights), function(j)
     unlist(weights[[j]])-(lr/nmb)*unlist(nabla_w[[j]]))
   biases <- lapply(seq_along(biases), function(j)
@@ -202,7 +222,7 @@ update_mini_batch <- function(mini_batch, lr, biases, weights)
 }
 
 # Return gradient updates using back-propogation
-backprop <- function(x, y, biases, weights)
+backprop <- function(x, y, C, sizes, num_layers, biases, weights)
 {
   # Initialise updates with zero vectors
   listw <- sizes[1:length(sizes)-1] 
@@ -239,9 +259,10 @@ backprop <- function(x, y, biases, weights)
   # Second:
   # Backwards (update gradient using errors)
   # Last layer
-  sp <- sigmoid_prime(zs[[length(zs)]])
+  #sp <- sigmoid_prime(zs[[length(zs)]])
   # Gradients update depending on how big the error in prediction was
-  delta <- cost_derivative(activations[[length(activations)]], y) * sp
+  #delta <- cost_derivative(activations[[length(activations)]], y) * sp
+  delta <- cost_delta(method=C, z=zs[[length(zs)]], a=activations[[length(activations)]], y=y)
   nabla_b_backprop[[length(nabla_b_backprop)]] <- delta
   nabla_w_backprop[[length(nabla_w_backprop)]] <- delta %*% t(activations[[length(activations)-1]])
   # Second to second-to-last-layer
@@ -267,7 +288,7 @@ evaluate <- function(testing_data, biases, weights)
     test_data_chunk <- testing_data[[i]]
     test_x <- test_data_chunk[[1]]
     test_y <- test_data_chunk[[-1]]
-    predictions[i] <- which.max(feedforward(test_x))
+    predictions[i] <- which.max(feedforward(test_x, biases, weights))
     truths[i] <- which.max(test_y)
   }
   correct <- sum(mapply(function(x,y) x==y, predictions, truths))
@@ -281,33 +302,42 @@ evaluate <- function(testing_data, biases, weights)
 }
 
 
-################################################
-## RUN (Example Iris)
-################################################
-# Example on iris data
-train_test_split <- train_test_from_df(df = iris, predict_col_index = 5, train_ratio = 0.5)
-training_data <- train_test_split[[1]]
-testing_data <- train_test_split[[2]]
+##################################################################################################################
+## Example 1. RUN (MNIST) - 96% Accuracy
+##################################################################################################################
 
+
+# Here we have splits for train-test already
+# Train
+mnist <- read.table('http://www.pjreddie.com/media/files/mnist_train.csv', sep=",", header = FALSE)
+mnist$V1 <- factor(mnist$V1)
+training_data <- train_test_from_df(df = mnist, predict_col_index = 1, train_ratio = 1, scale_input = TRUE)[[1]]
+
+# Test
+mnist <- read.table('http://www.pjreddie.com/media/files/mnist_test.csv', sep=",", header = FALSE)
+mnist$V1 <- factor(mnist$V1)
+testing_data <- train_test_from_df(df = mnist, predict_col_index = 1, train_ratio = 1, scale_input = TRUE)[[1]]
+
+# Input and output neurons
 input_neurons <- length(training_data[[1]][[1]])
 output_neurons <- length(training_data[[1]][[-1]])
 
 # Step 1. Initialise nueral network (bias and weights for layers)
-# Tested only with one hidden layer
-create_neural_net <- neuralnetwork(c(input_neurons, 6, 4, output_neurons))
-
-sizes <- create_neural_net[[1]]
-num_layers <- create_neural_net[[2]]
-biases <- create_neural_net[[3]]
-weights <- create_neural_net[[4]]
+# MNIST: 784, 100, 10 (one hidden-layer)
+create_neural_net <- neuralnetwork(c(input_neurons, 100, output_neurons))
 
 # Step 2. Train NN using SGD
 trained_net <- SGD(training_data=training_data,
-                   epochs=1000, 
+                   testing_data=testing_data,
+                   epochs=30, 
                    mini_batch_size=10,
-                   lr=0.2,
-                   biases=biases, 
-                   weights=weights)
+                   lr=3,
+                   C='ce',
+                   sizes=create_neural_net[[1]],
+                   num_layers=create_neural_net[[2]],
+                   biases=create_neural_net[[3]], 
+                   weights=create_neural_net[[4]],
+                   verbose=TRUE)
 
 # Trained matricies:
 biases <- trained_net[[1]]
@@ -319,37 +349,72 @@ evaluate(training_data, biases, weights)
 evaluate(testing_data, biases, weights)
 
 ################################################
-## RUN (Example Breast Cancer)
+## Example 2. RUN (Iris)
+################################################
+
+train_test_split <- train_test_from_df(df = iris, predict_col_index = 5, train_ratio = 0.7, scale_input = TRUE)
+training_data <- train_test_split[[1]]
+testing_data <- train_test_split[[2]]
+
+input_neurons <- length(training_data[[1]][[1]])
+output_neurons <- length(training_data[[1]][[-1]])
+
+create_neural_net <- neuralnetwork(c(input_neurons, 16, 4, output_neurons))
+
+trained_net <- SGD(training_data=training_data,
+                   testing_data=testing_data,
+                   epochs=30, 
+                   mini_batch_size=10,
+                   lr=3,
+                   C='ce',
+                   sizes=create_neural_net[[1]],
+                   num_layers=create_neural_net[[2]],
+                   biases=create_neural_net[[3]], 
+                   weights=create_neural_net[[4]],
+                   verbose=TRUE)
+
+# Trained matricies:
+biases <- trained_net[[1]]
+weights <- trained_net[[-1]]
+
+?save
+
+
+# Accuracy (train)
+evaluate(training_data, biases, weights)
+# Accuracy (test)
+evaluate(testing_data, biases, weights)
+
+ 
+################################################
+## Example 3. RUN (Example Breast Cancer)
 ################################################
 #install.packages('mlbench')
 library(mlbench)
 data("BreastCancer")
 df_in <- as.data.frame(BreastCancer)
-df_in$Id <- NULL
+df_in$Id <- NULL  # Don't need ID
 
-train_test_split <- train_test_from_df(df = df_in, predict_col_index = 10, train_ratio = 0.5)
+train_test_split <- train_test_from_df(df = df_in, predict_col_index = 10, train_ratio = 0.7, scale_input = TRUE)
 training_data <- train_test_split[[1]]
 testing_data <- train_test_split[[2]]
 
 input_neurons <- length(training_data[[1]][[1]])
 output_neurons <- length(training_data[[1]][[-1]])
 
-# Step 1. Initialise nueral network (bias and weights for layers)
-# Tested only with one hidden layer
-create_neural_net <- neuralnetwork(c(input_neurons, 6, 4, output_neurons))
+create_neural_net <- neuralnetwork(c(input_neurons, 50, output_neurons))
 
-sizes <- create_neural_net[[1]]
-num_layers <- create_neural_net[[2]]
-biases <- create_neural_net[[3]]
-weights <- create_neural_net[[4]]
-
-# Step 2. Train NN using SGD
 trained_net <- SGD(training_data=training_data,
-                   epochs=100, 
-                   mini_batch_size=50,
-                   lr=0.2,
-                   biases=biases, 
-                   weights=weights)
+                   testing_data=testing_data,
+                   epochs=30, 
+                   mini_batch_size=10,
+                   lr=3,
+                   C='ce',
+                   sizes=create_neural_net[[1]],
+                   num_layers=create_neural_net[[2]],
+                   biases=create_neural_net[[3]], 
+                   weights=create_neural_net[[4]],
+                   verbose=TRUE)
 
 # Trained matricies:
 biases <- trained_net[[1]]
@@ -359,6 +424,3 @@ weights <- trained_net[[-1]]
 evaluate(training_data, biases, weights)
 # Accuracy (test)
 evaluate(testing_data, biases, weights)
-
-
-
